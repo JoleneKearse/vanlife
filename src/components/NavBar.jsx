397f22c30b0fb320bca7da36ef87cf170a36a87f
JoleneKearse <pharaohnutz@gmail.com>
2025-02-27 10:38:38 -0500

    chore: add link in logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,7 +4,7 @@ const NavBar = () => {
   return (
     <nav className="bg-brand-100 text-neutral-300 py-14 flex justify-between px-10">
       <div>
-        <h3 className="font-black uppercase text-neutral-600 text-2xl">#VanLife</h3>
+        <Link to="/" className="font-black uppercase text-neutral-600 text-2xl">#VanLife</Link>
       </div>
       <div className="flex gap-4">
         <Link to="About" className="font-semibold">About</Link>
