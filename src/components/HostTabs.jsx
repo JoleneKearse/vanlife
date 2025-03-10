65631f5dc058e81c2e568fc3f67dd9b41a246539
JoleneKearse <pharaohnutz@gmail.com>
2025-03-10 11:48:34 -0400

    feat: add active links

--- src/components/HostTabs.jsx
+++ src/components/HostTabs.jsx
@@ -1,16 +1,53 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const HostTabs = () => {
   return (
     <nav className="mb-14">
       <ul className="flex gap-8 text-neutral-500 text-lg underline-offset-6">
-        <li><Link to="/host">Dashboard</Link></li>
-        <li><Link to="/host/income">Income</Link></li>
-        <li><Link to="/host/vans">Vans</Link></li>
-        <li><Link to="/host/reviews">Reviews</Link></li>
+        <li>
+          <NavLink
+            to="/host"
+            end
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Dashboard
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/host/income"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Income
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/host/vans"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Vans
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/host/reviews"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Reviews
+          </NavLink>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default HostTabs;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,46 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <header>
       <nav className="bg-brand-100 text-neutral-300 py-14 flex justify-between px-10">
         <div>
-          <Link to="/" className="font-black uppercase text-neutral-600 text-2xl">#VanLife</Link>
+          <NavLink
+            to="/"
+            className="font-black uppercase text-neutral-600 text-2xl"
+          >
+            #VanLife
+          </NavLink>
         </div>
         <div className="flex gap-4 font-semibold">
-          <Link to="/host">Host</Link>
-          <Link to="about">About</Link>
-          <Link to="vans">Vans</Link>
+          <NavLink
+            to="/host"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Host
+          </NavLink>
+          <NavLink
+            to="about"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="vans"
+            className={({ isActive }) =>
+              isActive ? "underline text-neutral-500 font-bold" : null
+            }
+          >
+            Vans
+          </NavLink>
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default NavBar;
