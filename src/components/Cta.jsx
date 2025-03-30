92514aeb04211261d68103b0862c6b89e802f027
JoleneKearse <pharaohnutz@gmail.com>
2025-03-30 15:22:20 -0400

    chore: add link to back to vans page

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "./Button";
 
 const Cta = () => {
@@ -5,9 +6,18 @@ const Cta = () => {
     <article className="bg-brand-300 p-8 rounded-sm mt-4 mb-16">
       <p className="text-2xl font-bold w-1/2">Your destination is waiting.</p>
       <p className="text-2xl font-bold w-1/2">Your van is ready.</p>
-      <Button btnColor="bg-neutral-500" btnTextColor="text-neutral-100" btnWeight="font-bold" BtnWidth="w-2/5">Explore our vans</Button>
+      <Link to="/vans">
+        <Button
+          btnColor="bg-neutral-500"
+          btnTextColor="text-neutral-100"
+          btnWeight="font-bold"
+          BtnWidth="w-2/5"
+        >
+          Explore our vans
+        </Button>
+      </Link>
     </article>
   );
-}
+};
 
 export default Cta;
