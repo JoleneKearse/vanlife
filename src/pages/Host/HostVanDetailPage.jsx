c5aad500c6d8e5d053201999f8eb2064e44cfd2d
JoleneKearse <pharaohnutz@gmail.com>
2025-04-13 14:00:47 -0400

    chore: add badge

--- src/pages/Host/HostVanDetailPage.jsx
+++ src/pages/Host/HostVanDetailPage.jsx
@@ -1,16 +1,17 @@
 import { useParams } from "react-router-dom";
-import HostLinkTo from "../../components/HostLinkTo";
 import { vans } from "../../data/vans";
+import HostLinkTo from "../../components/HostLinkTo";
 import Tab from "../../components/Tab";
+import Badge from "../../components/Badge";
+import { getBadgeColor } from "../../utils/utils";
 
 const HostVanDetailPage = () => {
   const { id } = useParams();
-  console.log(id);
   const van = vans.find((v) => v.id === parseInt(id));
   if (!van) {
     return <p>Van not found</p>;
   }
-  const { image, name, price } = van;
+  const { image, category, name, price } = van;
 
   return (
     <>
@@ -20,27 +21,22 @@ const HostVanDetailPage = () => {
       </HostLinkTo>
       <section className="flex gap-4 items-center my-8">
         <img src={image} alt="Van" className="w-1/2 h-auto rounded-sm" />
-        <div>
+        <div className="flex flex-col gap-4">
+          <Badge badgeColor={getBadgeColor(category)}>{category}</Badge>
           <p className="text-3xl font-bold">{name}</p>
-          <p className="text-xl font-semibold">${price}/day</p>
+          <p className="text-xl font-semibold -mt-2">${price}/day</p>
         </div>
       </section>
       <nav className="mb-8">
         <ul className="flex gap-8 text-neutral-500 text-md underline-offset-6">
           <li>
-            <Tab to="/host/vans/:id">
-              Details
-            </Tab>
+            <Tab to="/host/vans/:id">Details</Tab>
           </li>
           <li>
-            <Tab to="/host/vans/:id/pricing">
-              Pricing
-            </Tab>
+            <Tab to="/host/vans/:id/pricing">Pricing</Tab>
           </li>
           <li>
-            <Tab to="/host/vans/:id/photos">
-              Photos
-            </Tab>
+            <Tab to="/host/vans/:id/photos">Photos</Tab>
           </li>
         </ul>
       </nav>
